8f98a835cc9e257ea06863cbee83fa4fe3c7d7d4
Tatsuya Fujita <tfujita@gmail.com>
2023-08-21 19:36:07 +0900

    up

--- renderer.js
+++ renderer.js
@@ -4,6 +4,8 @@ information.innerText = `This app is using Chrome (v${window.myApi.chrome()}), N
 document.getElementById('info2').innerText = window.myApi.hamachi;
 
 
+let listViewMain;
+
 // Window Onload ハンドラ
 window.addEventListener('DOMContentLoaded', onLoad);
 function onLoad() {
@@ -39,148 +41,121 @@ function onLoad() {
   });
 
   document.querySelector('#btn-add').addEventListener('click', async()=>{
+    
+    listViewMain = new ListView('.list-view', ['ゲーム名','ZIP名','メーカー','年度','マスタ','ドライバ']);
+/*    
     await initListView({
       target: '.list-view',
       numItems: 10000,
       columns: ['ゲーム名','ZIP名','メーカー','年度','マスタ','ドライバ'],
-
     });
-/*    
-    const fragment = document.createDocumentFragment('tbody');
-    for (let i=0; i<40000; i++) {
-      const tr = document.createElement('tr');
-      const td = document.createElement('td');
-      td.append(document.createTextNode('要素'+i));
-      const td2 = document.createElement('td');
-      td2.append(document.createTextNode('テキスト'));
-      const td3 = document.createElement('td');
-      td3.append(document.createTextNode('テキスト'));
-      const td4 = document.createElement('td');
-      td4.append(document.createTextNode('テキスト'));
-      const td5 = document.createElement('td');
-      td5.append(document.createTextNode('テキスト'));
-      const td6 = document.createElement('td');
-      td6.append(document.createTextNode('テキスト'));
-      const td7 = document.createElement('td');
-      td7.append(document.createTextNode('テキスト'));
-
-      tr.appendChild(td);
-      tr.appendChild(td2);
-      tr.appendChild(td3);
-      tr.appendChild(td4);
-      tr.appendChild(td5);
-      tr.appendChild(td6);
-      tr.appendChild(td7);
-      fragment.appendChild(tr);
-    }
-    document.querySelector('#mainList').append(fragment);
-    */
+*/
   });
+
+  document.querySelector('#btn-item1').addEventListener('click', ()=>{
+    listViewMain.changeItemCount(1000);
+  });
+  document.querySelector('#btn-item2').addEventListener('click', ()=>{
+    listViewMain.changeItemCount(30);
+  });
+  document.querySelector('#btn-item3').addEventListener('click', ()=>{
+    listViewMain.changeItemCount(2);
+  });
+
+
 };
 
 /**
- * リストビューの初期化
- * @param options {{
+ * リストビュー用クラス
  *  target: string, // 対象のセレクタ
- *  numItems: integer, // 行数
  *  columns: [string], // カラムラベル
- *  
- *  }}
  */
-async function initListView(options) {
+class ListView {
 
-  // props
-  let lastHeight; // リサイズ前のサイズ保持
-  let rowHeight;  // 列の高さ 
+  // コンストラクタ
+  constructor(target, columns) {
+    this.numItems = 0; // 保持項目数
+    this.lastHeight = 0; // リサイズ前のサイズ保持
+    this.rowHeight = 0; // 列の高さ
 
-  const list = document.querySelector(options.target);
-  list.numItems = options.numItems;
-  list.columns = options.columns;
-  list.classList.add('m-fujList');
+    this.header; //
+    this.body;   //
+    this.ul;
 
-  // カラムヘッダ追加
-  const columnHeader = document.createElement('header');
-  columnHeader.className = 'm-fujList__header';
-  options.columns.forEach(e=>{
-    const headerItem = document.createElement('div');
-    headerItem.className = 'm-fujList__headerItem';
-    headerItem.innerText = e;
-    columnHeader.appendChild(headerItem);
-  });
-  list.appendChild(columnHeader);
+    // DOM構成
+    this.list = document.querySelector(target);
+    this.list.classList.add('m-fujList');
 
-  // カラムボディ追加
-  const columnBody = document.createElement('main');
-  columnBody.className = 'm-fujList__body';
-  
-  const ul = document.createElement('ul');
-  ul.className = 'm-fujList__list';
-  columnBody.appendChild(ul);
-  list.appendChild(columnBody);
+    // カラムヘッダ追加
+    const columnHeader = document.createElement('header');
+    columnHeader.className = 'm-fujList__header';
+    columns.forEach(e=>{
+      const headerItem = document.createElement('div');
+      headerItem.className = 'm-fujList__headerItem';
+      headerItem.innerText = e;
+      columnHeader.appendChild(headerItem);
+    });
+    this.list.appendChild(columnHeader);
 
-  // 列の高さ取得
-  const li = document.createElement('li');
-  li.className = 'm-fujList__listItem';
-  li.append(document.createTextNode('要素'));
-  ul.appendChild(li);
-  rowHeight = li.offsetHeight;
-  li.remove();
+    // カラムボディ追加
+    this.body = document.createElement('main');
+    this.body.className = 'm-fujList__body';
+    this.ul = document.createElement('ul');
+    this.ul.className = 'm-fujList__list';
+    this.body.appendChild(this.ul);
+    this.list.appendChild(this.body);
 
-  /*
-  for (let i=0; i<10000; i++) {
+    // 行の高さ取得
     const li = document.createElement('li');
     li.className = 'm-fujList__listItem';
-    li.append(document.createTextNode('要素'+i));
-    ul.appendChild(li);
-  }*/
+    li.append(document.createTextNode('要素'));
+    this.ul.appendChild(li);
+    this.rowHeight = li.offsetHeight;
+    li.remove();
+    this.lastHeight = this.body.offsetHeight;
 
-  lastHeight = columnBody.offsetHeight;
-  update();
+    // イベント追加
+    window.addEventListener('resize', (e)=>{
+      if (this.lastHeight != this.body.offsetHeight) {
+        this.lastHeight = this.body.offsetHeight;
+        this.updateListView();
+      }
+    });
+    this.updateListView();
+  }
+  
 
-  window.addEventListener('resize', (e)=>{
-    if (lastHeight!=columnBody.offsetHeight) {
-      lastHeight = columnBody.offsetHeight;
-      //console.log('resized: '+lastHeight);
-      update();
-    }
-  });
-
-  function update() {
-    const numDOMs = ul.childElementCount; // 現在存在する要素数
-
+  updateListView() {
+    const numDOMs = this.ul.childElementCount; // 現在存在する要素数
     // 必要な要素数
-    const neededRows = Math.ceil(lastHeight / rowHeight);
-
-    //console.log(numDOMs,neededRows,);
-
+    const neededRows = Math.ceil(this.lastHeight / this.rowHeight);
     if (numDOMs < neededRows) {
       for (let i=numDOMs; i<neededRows; i++) {
         const li = document.createElement('li');
         li.className = 'm-fujList__listItem';
         li.append(document.createTextNode('要素'+i));
-        ul.appendChild(li);
+        this.ul.appendChild(li);
       }
     } else 
     if (numDOMs > neededRows) {
       for (let i=numDOMs-1; i>=neededRows; i--) {
-        ul.removeChild(ul.children[i]);
+        this.ul.removeChild(this.ul.children[i]);
       }
     }
-
   }
 
-
-
+  changeItemCount(newItemCount) {
+    console.log(newItemCount)
+  }
 }
 
+
 async function sendByApi(zip) {
-  //result = await window.myApi.api1(zip);
   result = await window.retrofireAPI.executeMAME({
     zipName: zip,
   });
 }
 async function sendByApiSoft(zip) {
-  //result = await window.myApi.api1(zip);
   result = await window.retrofireAPI.executeMAME({
     zipName: zip,
     softName: 'ys2'
