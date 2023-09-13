4e96988ecb9da062298c39833702fefa9f097c74
Tatsuya Fujita <tfujita@gmail.com>
2023-09-13 18:37:44 +0900

    keyboard

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 //-------------------------------------------------------------------
 // モジュール
 //-------------------------------------------------------------------
-const child_process = require('node:child_process');
-const path = require('node:path');
+const child_process = require("node:child_process");
+const path = require("node:path");
 const fs = require("node:fs");
 
-const { app, BrowserWindow, dialog, ipcMain, shell } = require('electron');
-const Store = require('electron-store');
+const { app, BrowserWindow, dialog, ipcMain, shell } = require("electron");
+const Store = require("electron-store");
 const store = new Store();
 
-const { validateBufferMIMEType } = require('validate-image-type');
-const sizeOf = require('image-size');
+const { validateBufferMIMEType } = require("validate-image-type");
+const sizeOf = require("image-size");
 
-const { CONSTS, rfConfig, rfProfiles } = require('./rfConfig');
+const { CONSTS, rfConfig, rfProfiles } = require("./rfConfig");
 
 //-------------------------------------------------------------------
 // 初期設定
 //-------------------------------------------------------------------
 app.disableHardwareAcceleration();
-
 
 //-------------------------------------------------------------------
 // 定数
@@ -29,7 +28,7 @@ const MAIN_FORM_DEFAULT = {
   y: 50,
   width: 1024,
   height: 768,
-}
+};
 
 //-------------------------------------------------------------------
 // 変数
@@ -48,10 +47,9 @@ let settingsToBeStored = {};
  * ウインドウ生成
  */
 const createWindow = () => {
-
   // 保存値レストア
-  const pos = store.get('mainWindow.pos') || [MAIN_FORM_DEFAULT.x, MAIN_FORM_DEFAULT.y];
-  const size = store.get('mainWindow.size') || [MAIN_FORM_DEFAULT.width, MAIN_FORM_DEFAULT.height];
+  const pos = store.get("mainWindow.pos") || [MAIN_FORM_DEFAULT.x, MAIN_FORM_DEFAULT.y];
+  const size = store.get("mainWindow.size") || [MAIN_FORM_DEFAULT.width, MAIN_FORM_DEFAULT.height];
 
   // ウインドウ生成
   mainWindow = new BrowserWindow({
@@ -65,49 +63,53 @@ const createWindow = () => {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js'),
-    }
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   // ウィンドウ内に指定HTMLを表示
-  mainWindow.loadFile('index.html');
+  mainWindow.loadFile("index.html");
   mainWindow.webContents.openDevTools();
 
   // 準備が整ったら表示
-  mainWindow.once('ready-to-show', () => {
+  mainWindow.once("ready-to-show", () => {
     mainWindow.show();
   });
 
-  // ウィンドウが閉じられる直前に実行
-  mainWindow.on('close', async ()=>{
-    store.set('mainWindow.pos', mainWindow.getPosition());  // ウィンドウの座標を記録
-    store.set('mainWindow.size', mainWindow.getSize());     // ウィンドウのサイズを記録
-    
-    for (const [key, value] of Object.entries(settingsToBeStored)) { // 受信済み設定
-      store.set(key, value);
-    }
-
+  // ウインドウ閉じる直前
+  mainWindow.on("close", async () => {
+    console.log("mainwindow on close");
+    store.set("mainWindow.pos", mainWindow.getPosition()); // ウィンドウの座標を記録
+    store.set("mainWindow.size", mainWindow.getSize()); // ウィンドウのサイズを記録
   });
 
-}
+  // ウィンドウ閉じた後
+  mainWindow.on("closed", async () => {
+    console.log("mainwindow on closed");
+    for (const [key, value] of Object.entries(settingsToBeStored)) {
+      // 受信済み設定
+      store.set(key, value);
+    }
+  });
+};
 
 /**
  * resource.json を読み込む
  * @returns {boolean} - 読み込み完了
  */
 const loadResource = () => {
-  if ( fs.existsSync(CONSTS.PATH_RESOURCES)) {
+  if (fs.existsSync(CONSTS.PATH_RESOURCES)) {
     try {
-      recordString = fs.readFileSync(CONSTS.PATH_RESOURCES, 'utf8');
+      recordString = fs.readFileSync(CONSTS.PATH_RESOURCES, "utf8");
       return true;
-    } catch(err) {
+    } catch (err) {
       console.log(err);
       return false;
     }
   } else {
     return false;
   }
-}
+};
 
 /**
  * mame32j.lst を読み込む
@@ -116,8 +118,9 @@ const loadResource = () => {
 const loadMame32j = () => {
   if (fs.existsSync(CONSTS.PATH_MAME32J)) {
     try {
-      mame32jString = fs.readFileSync(CONSTS.PATH_MAME32J, 'utf8');
-      if (mame32jString.charCodeAt(0) === 0xFEFF) { // BOM削除
+      mame32jString = fs.readFileSync(CONSTS.PATH_MAME32J, "utf8");
+      if (mame32jString.charCodeAt(0) === 0xfeff) {
+        // BOM削除
         mame32jString = mame32jString.substring(1);
       }
       return true;
@@ -128,7 +131,7 @@ const loadMame32j = () => {
   } else {
     return false;
   }
-}
+};
 
 //------------------------------------
 // [app] イベント処理
@@ -136,31 +139,29 @@ const loadMame32j = () => {
 
 // 初期化が完了
 app.whenReady().then(async () => {
+  var tick = Date.now();
+  loadResource();
+  console.log("loadResource:", Date.now() - tick, "ms");
 
   var tick = Date.now();
-  loadResource(); 
-  console.log("loadResource:", Date.now() - tick, "ms");  
-
-  var tick = Date.now();
-  loadMame32j(); 
-  console.log("loadMame32j:", Date.now() - tick, "ms");  
+  loadMame32j();
+  console.log("loadMame32j:", Date.now() - tick, "ms");
 
   createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
-
-})
+});
 
 // すべてのウィンドウが閉じられたときの処理
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
 /**
  * MAME 起動処理
@@ -168,8 +169,7 @@ app.on('window-all-closed', () => {
  * @return { string }
  */
 
-const executeMAME = async(event, ...args) => {
-  
+const executeMAME = async (event, ...args) => {
   // プロファイル未選択時
   if (rfConfig.currentProfile == -1) {
     sendDebug("executeMAME(): No Profile Set.");
@@ -186,29 +186,29 @@ const executeMAME = async(event, ...args) => {
     mameArgs.push(rfProfiles[rfConfig.currentProfile].option);
   }
 
-  const subprocess  = child_process.spawn( rfProfiles[rfConfig.currentProfile].exePath, mameArgs, 
-    { cwd: rfProfiles[rfConfig.currentProfile].workDir,
-      detached: true,
-     });
+  const subprocess = child_process.spawn(rfProfiles[rfConfig.currentProfile].exePath, mameArgs, {
+    cwd: rfProfiles[rfConfig.currentProfile].workDir,
+    detached: true,
+  });
 
-  sendDebug( rfProfiles[rfConfig.currentProfile].exePath + ' ' + mameArgs.join(' '));
+  sendDebug(rfProfiles[rfConfig.currentProfile].exePath + " " + mameArgs.join(" "));
   subprocess.unref();
-  subprocess.stdout.setEncoding('utf8');
-  subprocess.stdout.on('data', (data)=> {
+  subprocess.stdout.setEncoding("utf8");
+  subprocess.stdout.on("data", (data) => {
     console.log(data);
     sendDebug(data);
   });
-  subprocess.stderr.setEncoding('utf8');
-  subprocess.stderr.on('data',(data)=>{
+  subprocess.stderr.setEncoding("utf8");
+  subprocess.stderr.on("data", (data) => {
     console.log(data);
     sendDebug(data);
   });
-  subprocess.on('close', (code)=>{
+  subprocess.on("close", (code) => {
     console.log(`child process exited with code ${code}`);
     //sendDebug(`child process exited with code ${code}`);
   });
 
-  subprocess.on('error', (err)=>{
+  subprocess.on("error", (err) => {
     shell.beep();
     sendDebug(`${err}`);
     //sendDebug(`child process exited with code ${code}`);
@@ -224,21 +224,20 @@ const executeMAME = async(event, ...args) => {
  */
 async function openLocalImage(path) {
   let err = false;
-  let errorMessage = '';
+  let errorMessage = "";
   let enc;
-  let img;  
+  let img;
   let dimensions;
 
   try {
     img = fs.readFileSync(path); // 開く
-    const validate = await validateBufferMIMEType( img,
-                  {allowMimeTypes: ['image/jpeg', 'image/gif', 'image/png']}); // 画像ファイル検証
+    const validate = await validateBufferMIMEType(img, { allowMimeTypes: ["image/jpeg", "image/gif", "image/png"] }); // 画像ファイル検証
     if (validate.ok) {
       dimensions = sizeOf(img);
-      enc = img.toString('base64'); // base64
+      enc = img.toString("base64"); // base64
     } else {
       err = true;
-      errorMessage = 'INVALID IMAGE FILE.';
+      errorMessage = "INVALID IMAGE FILE.";
     }
   } catch (e) {
     err = true;
@@ -246,41 +245,41 @@ async function openLocalImage(path) {
   }
 
   if (err) {
-    return {result: false, error: errorMessage};
+    return { result: false, error: errorMessage };
   } else {
-    return {result: true, img: enc, type: dimensions.type, width: Number(dimensions.width), height: Number(dimensions.height)};
+    return {
+      result: true,
+      img: enc,
+      type: dimensions.type,
+      width: Number(dimensions.width),
+      height: Number(dimensions.height),
+    };
   }
-
 }
-
 
 /**
  * デバッグメッセージ送信
  */
 function sendDebug(text) {
-  mainWindow.webContents.send('debug-message', text);
+  mainWindow.webContents.send("debug-message", text);
 }
-
 
 //------------------------------------
 // ipc通信
 //------------------------------------
-ipcMain.handle('window-reset', async(event, data)=>{
+ipcMain.handle("window-reset", async (event, data) => {
   console.log(data);
   mainWindow.setSize(MAIN_FORM_DEFAULT.width, MAIN_FORM_DEFAULT.height);
   mainWindow.setPosition(MAIN_FORM_DEFAULT.x, MAIN_FORM_DEFAULT.y);
   sendDebug("ウインドウリセット");
-  return(true);
+  return true;
 });
 
-ipcMain.handle('open-dialog', async(event, data)=>{
-
+ipcMain.handle("open-dialog", async (event, data) => {
   const result = dialog.showOpenDialogSync(mainWindow, {
-    title: 'ｶﾞｿﾞｳｦｾﾝﾀｸｾﾖ',
-    properties: ['openFile'],
-    filters: [
-      { name: 'Images', extensions: ['jpg', 'png', 'gif'] },
-    ]
+    title: "ｶﾞｿﾞｳｦｾﾝﾀｸｾﾖ",
+    properties: ["openFile"],
+    filters: [{ name: "Images", extensions: ["jpg", "png", "gif"] }],
   });
 
   if (result) {
@@ -289,25 +288,24 @@ ipcMain.handle('open-dialog', async(event, data)=>{
 });
 
 // ゲーム情報を返す
-ipcMain.handle('get-record', async(event, data)=>{
+ipcMain.handle("get-record", async (event, data) => {
   return recordString;
 });
 
 // mame32j.lstを返す
-ipcMain.handle('get-mame32j', async(event, data)=>{
+ipcMain.handle("get-mame32j", async (event, data) => {
   return mame32jString;
 });
 
-
 /**
- * MAME 起動処理 
+ * MAME 起動処理
  */
-ipcMain.handle('execute-MAME', executeMAME);
+ipcMain.handle("execute-MAME", executeMAME);
 
 /**
  * Store の値を返す
  */
-ipcMain.handle('get-store', async (event, data) =>{
+ipcMain.handle("get-store", async (event, data) => {
   if (store.has(data)) {
     return store.get(data);
   } else {
@@ -318,14 +316,14 @@ ipcMain.handle('get-store', async (event, data) =>{
 /**
  * Store の値を保存
  */
-ipcMain.handle('set-store', async(event, data) =>{
+ipcMain.handle("set-store", async (event, data) => {
   store.set(data.key, data.val);
 });
 
 /**
  * Store の値を一時保持
  */
-ipcMain.handle('set-store-temp', async(event, data) =>{
+ipcMain.handle("set-store-temp", async (event, data) => {
+  console.log("setting received", data);
   settingsToBeStored[data.key] = data.val;
 });
-
--- renderer.js
+++ renderer.js
@@ -1,148 +1,148 @@
-'use strict';
+"use strict";
 
 let listViewMain; // メインリストビュー
 let record; // オリジナルの全ゲーム情報
 
-const LANG = { JP: 0, EN: 1,};
+const LANG = { JP: 0, EN: 1 };
 let config = {
   language: LANG.JP, // 言語設定
-  searchWord: '',
-  searchTarget: '',
-  zipName: '',
-  dataIndex: -1,
-}
+  searchWord: "", // 検索文字列
+  searchTarget: "", // 検索対象
+};
 
 // Window Onload
-window.addEventListener('DOMContentLoaded', onLoad);
+window.addEventListener("DOMContentLoaded", onLoad);
+
 async function onLoad() {
-
   // 設定読み込みと適用
-  const readConfig = await window.retrofireAPI.getStore('config');
+  const readConfig = await window.retrofireAPI.getStore("config");
   if (readConfig) {
-    if (readConfig.hasOwnProperty('searchWord')) {
+    if (readConfig.hasOwnProperty("searchWord")) {
       config.searchWord = readConfig.searchWord;
-      document.getElementById('search').value = readConfig.searchWord;
-    }
-  
-    if (readConfig.hasOwnProperty('language')) {
-      config.language = readConfig.language;
-      document.getElementById('language').checked = (readConfig.language == LANG.EN);
-    }
-  
-    if (readConfig.hasOwnProperty('searchTarget')) {
-      config.searchTarget = readConfig.searchTarget;
-      document.querySelector('input[name="searchRadio"][value="'+readConfig.searchTarget+'"]').checked = true;
+      document.getElementById("search").value = readConfig.searchWord;
     }
 
-    if (readConfig.hasOwnProperty('zipName')) {
-      config.zipName = readConfig.zipName;
+    if (readConfig.hasOwnProperty("language")) {
+      config.language = readConfig.language;
+      document.getElementById("language").checked = readConfig.language == LANG.EN;
+    }
+
+    if (readConfig.hasOwnProperty("searchTarget")) {
+      config.searchTarget = readConfig.searchTarget;
+      document.querySelector('input[name="searchRadio"][value="' + readConfig.searchTarget + '"]').checked = true;
     }
   }
-  
 
   // キー入力処理
-  window.addEventListener('keydown', e => {
-
+  window.addEventListener("keydown", (e) => {
     switch (e.key) {
       case "F12": // 言語切替
-        config.language = (config.language==LANG.JP)?LANG.EN:LANG.JP;
-        document.getElementById('language').checked = (config.language == LANG.EN);
+        config.language = config.language == LANG.JP ? LANG.EN : LANG.JP;
+        document.getElementById("language").checked = config.language == LANG.EN;
         listViewMain.updateListView(true);
-        saveFormConfig();
-      break;
+        break;
       case "Backspace": // 検索ボックスフォーカス
-        const search = document.getElementById('search'); 
-        if (e.target !== search ) {
+        const search = document.getElementById("search");
+        if (e.target !== search) {
           search.focus();
-          search.select();
           e.preventDefault();
         }
-      break;
+        break;
       case "Escape": // 検索リセット
         clearSearch();
-      break;
+        break;
       case "F5": // 起動
       case "F9":
-        if (listViewMain.zipName !== '') {
+        if (listViewMain.zipName !== "") {
           window.retrofireAPI.executeMAME({ zipName: listViewMain.zipName });
         }
-      break;
+        break;
     }
-
   });
 
   // 言語切替
-  document.getElementById('language').addEventListener('change', e=>{
-    
-    config.language = (e.target.checked)?LANG.EN:LANG.JP;
+  document.getElementById("language").addEventListener("change", (e) => {
+    config.language = e.target.checked ? LANG.EN : LANG.JP;
     listViewMain.updateListView(true);
-    saveFormConfig();
-  })
+  });
 
   // empty the debug output
-  document.querySelector('#debug').value = '';
+  document.querySelector("#debug").value = "";
 
-  document.querySelector('#test1').addEventListener('click', () => {
-    sendByApi(document.querySelector('#test1').value);
+  document.querySelector("#test1").addEventListener("click", () => {
+    sendByApi(document.querySelector("#test1").value);
   });
-  document.querySelector('#test2').addEventListener('click', () => {
-    sendByApi(document.querySelector('#test2').value);
+  document.querySelector("#test2").addEventListener("click", () => {
+    sendByApi(document.querySelector("#test2").value);
   });
-  document.querySelector('#test3').addEventListener('click', () => {
-    sendByApiSoft(document.querySelector('#test3').value);
+  document.querySelector("#test3").addEventListener("click", () => {
+    sendByApiSoft(document.querySelector("#test3").value);
   });
 
-  document.querySelector('#btn-reset').addEventListener('click', async()=>{
+  document.querySelector("#btn-reset").addEventListener("click", async () => {
     // メインプロセスを呼び出し
-    result = await window.retrofireAPI.resetWindow('reset');
+    result = await window.retrofireAPI.resetWindow("reset");
     console.log(result);
   });
 
-  document.querySelector('#btn-dialog').addEventListener('click', async()=>{
+  document.querySelector("#btn-dialog").addEventListener("click", async () => {
     // メインプロセスを呼び出し
-    const result = await window.retrofireAPI.dialog('');
+    const result = await window.retrofireAPI.dialog("");
     if (result && result.result == true) {
-      document.querySelector('#openImage').src = 'data:image/png;base64,'+result.img;
+      document.querySelector("#openImage").src = "data:image/png;base64," + result.img;
     }
   });
 
-  document.querySelector('#btn-item1').addEventListener('click', ()=>{
-
-  });
-  document.querySelector('#btn-item2').addEventListener('click', ()=>{
+  document.querySelector("#btn-item1").addEventListener("click", () => {});
+  document.querySelector("#btn-item2").addEventListener("click", () => {
     console.log(listViewMain.dataIndex);
     listViewMain.makeVisible();
   });
 
   // 検索欄入力イベント
-  document.querySelector("#search").addEventListener('input', e=>{
-    if (e.target.getAttribute('IME') !== 'true') {
-      config.searchWord = e.target.value;
-      listViewMain.updateListViewSearch();
-      saveFormConfig();
+  document.querySelector("#search").addEventListener("input", (e) => {
+    if (e.target.getAttribute("IME") !== "true") {
+      listViewMain.updateListViewSearch({ searchWord: e.target.value });
     }
   });
 
+  document.querySelector("#search").addEventListener("keydown", (e) => {
+    if (e.code === "Tab") {
+      listViewMain.setFocusOnItem();
+      e.preventDefault();
+      return;
+    }
+
+    if (e.target.getAttribute("IME") == "true") {
+    } else {
+      if (e.code === "Enter" || e.code === "NumpadEnter") {
+        listViewMain.setFocusOnItem();
+        e.preventDefault();
+        return;
+      }
+    }
+  });
+
+  document.querySelector("#search").addEventListener("focus", (e) => {
+    e.target.select();
+  });
+
   // IME 変換中
-  document.querySelector("#search").addEventListener('compositionstart', e=>{
-    e.target.setAttribute('IME', true);
+  document.querySelector("#search").addEventListener("compositionstart", (e) => {
+    e.target.setAttribute("IME", true);
   });
 
   // IME 変換確定
-  document.querySelector("#search").addEventListener('compositionend', e=>{
-    e.target.setAttribute('IME', false);
-    //console.log('compositionend:', e.target.value);
-    config.searchWord = e.target.value;
-    listViewMain.updateListViewSearch();
-    saveFormConfig();
+  document.querySelector("#search").addEventListener("compositionend", (e) => {
+    e.target.setAttribute("IME", false);
+    listViewMain.updateListViewSearch({ searchWord: e.target.value });
   });
 
   // 検索対象
-  document.getElementsByName('searchRadio').forEach(item => {
-    item.addEventListener('change', e=>{
+  document.getElementsByName("searchRadio").forEach((item) => {
+    item.addEventListener("change", (e) => {
       config.searchTarget = document.querySelector('input[name="searchRadio"]:checked').value;
-      listViewMain.updateListViewSearch();
-      saveFormConfig();
+      listViewMain.updateListViewSearch({ searchTarget: config.searchTarget });
     });
   });
 
@@ -150,70 +150,106 @@ async function onLoad() {
   var tick = Date.now();
   record = JSON.parse(await window.retrofireAPI.getRecord());
   // descJ と kana 追加
-  for(let i=0; i<record.length;i++) {
+  for (let i = 0; i < record.length; i++) {
     record[i].kana = record[i].desc;
     record[i].descJ = record[i].desc;
-  } 
+  }
   console.log(Date.now() - tick);
-  console.log("resources.json:",record.length);
+  console.log("resources.json:", record.length);
 
   var tick = Date.now();
   let mame32j = await window.retrofireAPI.getMame32j();
   mame32j = mame32j.split("\r\n");
-  
+
   let n = 0;
-  for(let i=0; i<mame32j.length;i++) {
+  for (let i = 0; i < mame32j.length; i++) {
     const item = mame32j[i].split("\t");
-    for (let j=n; j<record.length; j++) {
+    for (let j = n; j < record.length; j++) {
       if (record[j].zipname === item[0]) {
         record[j].descJ = item[1];
         record[j].kana = item[2];
-        n = j+1;
+        n = j + 1;
         break;
       }
     }
   }
-  console.log("mame32j:", Date.now() - tick,"ms");
+  console.log("mame32j:", Date.now() - tick, "ms");
 
   // リストビュー初期化
   var tick = Date.now();
   listViewMain = new ListView({
     data: record,
-    target: '.list-view',
+    target: ".list-view",
     columns: [
-      {label:'ゲーム名', data: "desc", order: 0, width: 380, defaultSort: "asc"},
-      {label:'ZIP名', data: "zipname", order: 1, width: 100, defaultSort: "asc"},
-      {label:'メーカー', data: "maker", order:2, width: 160, defaultSort: "asc"},
-      {label:'年度', data: "year", order: 3, width: 55, defaultSort: "asc"},
-      {label:'マスタ', data: "cloneof", order: 4, width: 100, defaultSort: "asc"},
-      {label:'ドライバ', data: "source", order: 5, width: 180, defaultSort: "asc"},
+      {
+        label: "ゲーム名",
+        data: "desc",
+        order: 0,
+        width: 380,
+        defaultSort: "asc",
+      },
+      {
+        label: "ZIP名",
+        data: "zipname",
+        order: 1,
+        width: 100,
+        defaultSort: "asc",
+      },
+      {
+        label: "メーカー",
+        data: "maker",
+        order: 2,
+        width: 160,
+        defaultSort: "asc",
+      },
+      { label: "年度", data: "year", order: 3, width: 55, defaultSort: "asc" },
+      {
+        label: "マスタ",
+        data: "cloneof",
+        order: 4,
+        width: 100,
+        defaultSort: "asc",
+      },
+      {
+        label: "ドライバ",
+        data: "source",
+        order: 5,
+        width: 180,
+        defaultSort: "asc",
+      },
     ],
-    slug: 'main',
+    slug: "main",
     orderByIndex: 1,
     sortDirection: "asc",
     index: -1,
-    zipName: config.zipName,
+    searchWord: config.searchWord,
+    searchTarget: config.searchTarget,
   });
+
   await listViewMain.init();
-  console.log('listview init:', Date.now() - tick,"ms");
+  console.log("listview init:", Date.now() - tick, "ms");
 }
+
+// ウインドウ終了前
+window.addEventListener("beforeunload", (e) => {
+  saveFormConfig();
+  listViewMain.saveSettings();
+});
 
 // 検索クリア
-document.getElementById('clear').addEventListener('click', clearSearch);
+document.getElementById("clear").addEventListener("click", clearSearch);
 function clearSearch() {
-  config.searchWord = "";
   document.querySelector("#search").value = "";
-  listViewMain.updateListViewSearch();
-  saveFormConfig();
+  listViewMain.updateListViewSearch({ searchWord: "" });
 }
 
-// フォームのconfig送信
+// フォームの config 送信
 function saveFormConfig() {
+  console.log("saveFormConfig: config.zipName", config.zipName);
   try {
-    config.zipName = listViewMain.zipName;
-    config.dataIndex = listViewMain.dataIndex;
-    window.retrofireAPI.setStoreTemp({key: "config", val: config});
-    console.log('config sent to main.js');
+    config.searchWord = document.querySelector("#search").value.trim();
+    window.retrofireAPI.setStoreTemp({ key: "config", val: config });
+    console.log("フォーム設定 main.js に送信");
   } catch (e) {
     console.log(e);
   }
@@ -225,21 +261,20 @@ async function sendByApi(zip) {
     zipName: zip,
   });
 }
+
 async function sendByApiSoft(zip) {
   result = await window.retrofireAPI.executeMAME({
     zipName: zip,
-    softName: 'ys2'
+    softName: "ys2",
   });
 }
-
-
 
 //------------------------------------
 // メインスレッドから受信
 //------------------------------------
 window.retrofireAPI.onDebugMessage((_event, text) => {
   console.log("onDebugMessage", text);
-  const debug = document.querySelector('#debug');
+  const debug = document.querySelector("#debug");
   debug.value = debug.value + text + "\n";
   debug.scrollTop = debug.scrollHeight;
 });
